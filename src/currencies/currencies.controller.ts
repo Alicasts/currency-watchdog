d119c46a72093024a7efa2bee64474edd299a31f
Ali <ali.cast.santos@gmail.com>
2025-04-04 12:46:24 -0300

    Implement fetch and persistence

--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,15 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { FetchAvailableCurrenciesUseCase } from './use-cases/fetch-available-currencies/fetch-available-currencies.use-case';
+import { LoadAvailableCurrenciesUseCase } from './use-cases/fetch-available-currencies/fetch-available-currencies.use-case';
 import { AvailableCurrencyDto } from './dto/available-currency.dto';
 
 @Controller('currencies')
 export class CurrenciesController {
     constructor(
-        private readonly fetchAvailableCurrenciesUseCase: FetchAvailableCurrenciesUseCase,
+        private readonly loadAvailableCurrenciesUseCase: LoadAvailableCurrenciesUseCase,
     ) {}
 
     @Get('available')
     async getAvailableCurrencies(): Promise<AvailableCurrencyDto[]> {
-        return this.fetchAvailableCurrenciesUseCase.execute();
+        return this.loadAvailableCurrenciesUseCase.execute();
     }
 }
--- src/currencies/use-cases/fetch-available-currencies/fetch-available-currencies.use-case.ts
+++ src/currencies/use-cases/fetch-available-currencies/fetch-available-currencies.use-case.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CurrenciesService } from 'src/currencies/currencies.service';
 import { AvailableCurrencyDto } from 'src/currencies/dto/available-currency.dto';
+import { AvailableCurrenciesRepository } from 'src/currencies/repositories/available-currencies.repository';
 
 @Injectable()
-export class FetchAvailableCurrenciesUseCase {
-  constructor(private readonly currenciesService: CurrenciesService) {}
+export class LoadAvailableCurrenciesUseCase {
+  constructor(
+    private readonly currenciesService: CurrenciesService,
+    private readonly availableCurrenciesRepository: AvailableCurrenciesRepository,) {}
 
-  async execute(): Promise<AvailableCurrencyDto[]> {
-    const result = await this.currenciesService.fetchAvailableCurrencies();
-
-    return Object.entries(result).map(
-      ([code, label]) => new AvailableCurrencyDto(code, label)
-    );
-  }
+    async execute(): Promise<AvailableCurrencyDto[]> {
+      const isCacheValid = await this.availableCurrenciesRepository.isCacheValid();
+  
+      if (isCacheValid) {
+        return this.availableCurrenciesRepository.getAll();
+      }
+  
+      const result = await this.currenciesService.fetchAvailableCurrencies();
+  
+      const currencies = Object.entries(result).map(
+        ([code, label]) => new AvailableCurrencyDto(code, label),
+      );
+  
+      await this.availableCurrenciesRepository.saveAll(currencies);
+  
+      return currencies;
+    }
 }
